fc5f058820be4d6e9414c6d7ee6a04b829aca253
Keith Vuong <keithwong2205@gmail.com>
2019-09-17 18:43:14 +0700

    CleanUp

--- src/com/keith/oop/StackingBoard.java
+++ src/com/keith/oop/StackingBoard.java
@@ -1,15 +1,15 @@
 package com.keith.oop;
 
 import com.keith.oop.PlayableBrick.Brick;			//Import Brick form PlayableBrick to add to the GameBoard
-import javax.swing.JLabel;									
-import javax.swing.JPanel;
-import javax.swing.Timer;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import javax.swing.JLabel;					//JLabel for Status Bar				
+import javax.swing.JPanel;					//JPanel for UI
+import javax.swing.Timer;					//Timer for GamePlay
+import java.awt.Color;						//Color for UI
+import java.awt.Graphics;					//Graphics for UI
+import java.awt.event.ActionEvent;				//ActionEvent for Game Event
+import java.awt.event.ActionListener;				//ActionListener for ActionEvent
+import java.awt.event.KeyAdapter;				//KeyAdapter for key press
+import java.awt.event.KeyEvent;					//KeyEvent for ActionPerform
 
 public class StackingBoard extends JPanel 
 {
@@ -21,77 +21,77 @@ public class StackingBoard extends JPanel
 	private final int Width = 12;							//Setting the Width of the stacking board
 	private final int Duration = 400;						//Speed of the game (Higher number = slower brick and vice versa)
 	
-	private Timer timer;									//Timer for the Game
-	private boolean stopFalling = false;					//Brick falling state
+	private Timer timer;								//Timer for the Game
+	private boolean stopFalling = false;						//Brick falling state
 	private boolean Paused = false;							//Game state
 	private int ClearedLine = 0;							//Score
 	private int BrickCurrentX = 0;							//Brick current X coordinate
 	private int BrickCurrentY = 0;							//Brick current Y coordinate
-	private JLabel StatsBar;								//StatsBar
-	private PlayableBrick CurrentBrick;						//Brick  attributes inherit from PlayableBrick
-	private Brick[] gameboard;								//GameBoard array storing Bricks
+	private JLabel StatsBar;							//StatsBar
+	private PlayableBrick CurrentBrick;						//Brick attributes inherit from PlayableBrick
+	private Brick[] gameboard;							//GameBoard array storing Bricks
 	
-	public StackingBoard(MainGameControl parent)			//Getting Control inheritance from MainGameControl
+	public StackingBoard(MainGameControl parent)					//Getting Control inheritance from MainGameControl
 	{
 		initStackingBoard(parent);
 	}
 	
-	private void initStackingBoard(MainGameControl parent)	//Initialize UI
+	private void initStackingBoard(MainGameControl parent)				//Initialize UI
 	{
-		setFocusable(true);									//Set Focus for Event
+		setFocusable(true);							//Set Focus for Event
 		StatsBar = parent.getStatsBar();					//Get StatsBar from MainGameControl
-		addKeyListener(new KeyRegcogniser());				//KeyListener for key press
+		addKeyListener(new KeyRegcogniser());					//KeyListener for key press
 	}
 	
-	private int BlockWidth() 								//Setting Width of blocks making up Bricks
+	private int BlockWidth() 							//Setting Width of blocks making up Bricks
 	{
 		return (int) getSize().getWidth() / Width;
 	}
 	private int BlockHeight()
 	{
-		return (int) getSize().getHeight() / Height;		//Setting Height of blocks making up Bricks
+		return (int) getSize().getHeight() / Height;				//Setting Height of blocks making up Bricks
 	}
 	
-	private Brick CurrentShapeAt(int x, int y)				//Getting the shape of the brick at a certain coordinates
+	private Brick CurrentShapeAt(int x, int y)					//Getting the shape of the brick at a certain coordinates
 	{
 		return gameboard[(y * Width) + x];
 	}
 	
-	void GameStart()										//GameStarting...
+	void GameStart()								//GameStarting...
 	{
 		CurrentBrick = new PlayableBrick();					//Setting new Brick
-		gameboard = new Brick[Width * Height];				//Setting new GameBoard
+		gameboard = new Brick[Width * Height];					//Setting new GameBoard
 		
-		CleanSlate();										//Clean the board
-		newBrick();											//Generate new Brick
+		CleanSlate();								//Clean the board
+		newBrick();								//Generate new Brick
 		
-		timer = new Timer(Duration, new GamePlay());		//New Timer for GamePlay
-		timer.start();										//Start Timer
+		timer = new Timer(Duration, new GamePlay());				//New Timer for GamePlay
+		timer.start();								//Start Timer
 	}
 	
-	private void GamePaused()								//GamePause...
+	private void GamePaused()							//GamePause...
 	{
 		Paused = !Paused;
 		
-		if (Paused)											//Paused!
+		if (Paused)								//Paused!
 		{
 			StatsBar.setText("Game is Paused !!");
-		} else												//Un-Paused
+		} else									//Un-Paused
 		{
 			var statlabel = String.format("Lines Cleared: %d", ClearedLine);
 			StatsBar.setText(statlabel);
 		}
-		repaint();											//Repaint StatsBar 
+		repaint();								//Repaint StatsBar 
 	}
 	
 	@Override 
-	public void paintComponent(Graphics g)				//Declare and initialize paintComponent for drawing
+	public void paintComponent(Graphics g)						//Declare and initialize paintComponent for drawing
 	{
 		super.paintComponent(g);
 		DrawBoard(g);
 	}
 	
-	private void DrawBoard(Graphics g)				//Drawing the Board for the game
+	private void DrawBoard(Graphics g)						//Drawing the Board for the game
 	{
 		var boardsize = getSize();
 		int boardTop = (int) boardsize.getHeight() - Height * BlockHeight();
@@ -120,13 +120,13 @@ public class StackingBoard extends JPanel
 		}
 	}
 	
-	private void FullDropDown()				//Drop the Brick straight down to the bottom
+	private void FullDropDown()							//Drop the Brick straight down to the bottom
 	{
 		int newY = BrickCurrentY;
 		
 		while (newY > 0)
 		{
-			if (!tryMovable(CurrentBrick, BrickCurrentX, newY - 1)) //This is to ensure that you ommit the drop 1 line
+			if (!tryMovable(CurrentBrick, BrickCurrentX, newY - 1)) 	//This is to ensure that you ommit the drop 1 line
 			{
 				break;
 			}
@@ -135,7 +135,7 @@ public class StackingBoard extends JPanel
 		BrickDropped();
 	}
 	
-	private void MoveDown()					//Move the Brick 1 line down
+	private void MoveDown()								//Move the Brick 1 line down
 	{
 		if (!tryMovable(CurrentBrick, BrickCurrentX, BrickCurrentY - 1))
 		{
@@ -143,7 +143,7 @@ public class StackingBoard extends JPanel
 		}
 	}
 	
-	private void CleanSlate()				//Clean the board
+	private void CleanSlate()							//Clean the board
 	{
 		for (int i = 0; i < Height * Width; i++)
 		{
@@ -151,7 +151,7 @@ public class StackingBoard extends JPanel
 		}
 	}
 			
-	private void BrickDropped()				//Function for moving (dropping the brick)
+	private void BrickDropped()							//Function for moving (dropping the brick)
 	{
 		for (int i=0; i<4; i++)
 		{
@@ -160,19 +160,19 @@ public class StackingBoard extends JPanel
 			gameboard[(y * Width) + x] = CurrentBrick.getShape();
 		}
 		ClearOneLine();
-		if (!stopFalling) 					//Create new Brick when old one stopped falling
+		if (!stopFalling) 							//Create new Brick when old one stopped falling
 		{
 			newBrick();
 		}
 	}
 	
-	private void newBrick()					//Generating a new Brick
+	private void newBrick()								//Generating a new Brick
 	{
 		CurrentBrick.setRandomShape();
 		BrickCurrentX = Width / 2 + 1;
 		BrickCurrentY = Height - 1 + CurrentBrick.minY();
 		
-		if (!tryMovable(CurrentBrick, BrickCurrentX, BrickCurrentY)) //This is when you can't move down the Brick anymore
+		if (!tryMovable(CurrentBrick, BrickCurrentX, BrickCurrentY)) 		//This is when you can't move down the Brick anymore
 		{
 			CurrentBrick.setShape(Brick.None);
 			timer.stop();
@@ -209,7 +209,7 @@ public class StackingBoard extends JPanel
 		return true;
 	}
 	
-	private void ClearOneLine()						//Clear full line when it is filled
+	private void ClearOneLine()							//Clear full line when it is filled
 	{
 		int LinesFilled = 0;
 		
@@ -249,7 +249,7 @@ public class StackingBoard extends JPanel
 		}
 	}
 	
-	private void DrawBlock(Graphics g, int x, int y, Brick shape)				//Drawing the blocks make up the Bricks
+	private void DrawBlock(Graphics g, int x, int y, Brick shape)			//Drawing the blocks make up the Bricks
 	{
 		Color BlockColors[] = 
 		{
@@ -277,7 +277,7 @@ public class StackingBoard extends JPanel
 		g.drawLine(x + BlockWidth() -1, y + BlockHeight() - 1, x + BlockWidth() - 1, y + 1);
 	}
 	
-	private class GamePlay implements ActionListener 			//GamePlay class inherits ActionListener
+	private class GamePlay implements ActionListener 				//GamePlay class inherits ActionListener
 	{
 
 		@Override
@@ -287,19 +287,19 @@ public class StackingBoard extends JPanel
 		}
 	}
 	
-	private void IgniteGamePlay()								//New GamePlay initialization
+	private void IgniteGamePlay()							//New GamePlay initialization
 	{	
-		update();												//Update new board
-		repaint();												//Re-painting new board
+		update();								//Update new board
+		repaint();								//Re-painting new board
 	}
 	
 	private void update()										
 	{
-		if (Paused)												//Paused
+		if (Paused)								//Paused
 		{
 			return;
 		}
-		if (stopFalling)										//When Brick finished falling
+		if (stopFalling)							//When Brick finished falling
 		{
 			stopFalling = false;
 			newBrick();
@@ -309,7 +309,7 @@ public class StackingBoard extends JPanel
 		}
 	}
 		
-	class KeyRegcogniser extends KeyAdapter				//Key Adapter for the control of the game
+	class KeyRegcogniser extends KeyAdapter						//Key Adapter for the control of the game
 	{
 		
 		@Override
